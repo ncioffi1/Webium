fede6518d7a3dfa3a586595ef9e98a7a391fea92
ncioffi1 <imnickcioffi@gmail.com>
2024-01-18 11:09:16 -0500

    frontend changes

--- frontend/src/components/SessionModal/SessionModal.jsx
+++ frontend/src/components/SessionModal/SessionModal.jsx
@@ -2,6 +2,8 @@
 import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import * as modalActions from '../../store/modals'
+import * as sessionActions from '../../store/session';
+
 import Modal from '../Modal/Modal';
 import { useNavigate, useParams } from 'react-router';
 
